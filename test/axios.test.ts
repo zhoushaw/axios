8838d520a6b03f4f7b47c454847313be8abaa5ab
sushi <codingzx@gmail.com>
2019-04-30 18:32:33 +0800

    feat(entery file):

--- test/axios.test.ts
+++ test/axios.test.ts
@@ -1,14 +1,14 @@
-import DummyClass from "../src/axios"
+import DummyClass from '../src/index'
 
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
 
-  it("DummyClass is instantiable", () => {
+  it('DummyClass is instantiable', () => {
     expect(new DummyClass()).toBeInstanceOf(DummyClass)
   })
 })
